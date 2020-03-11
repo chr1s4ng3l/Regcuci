639a724cd33075083c2cba06010014dfd59d8e40
chr1s4ng3l <christopher.tamayocazarez@gmail.com>
2020-03-10 19:54:51 -0600

    Regcuci
    Codigo fuente Java

---
+++ Registro.java
@@ -0,0 +1,15 @@
+
+package ventanas;
+
+
+public class Registro {
+     public long dir = 0;//dirección donde ire a buscar un registro  
+    public String llave = "";//campo llave  
+
+    public Registro(long dirx, String llavex) {
+        dir = dirx;
+        llave = llavex;
+
+    }
+    
+}
